fix cvs-adc2x gradients

adcc/testdata/dump_fdiff_gradient.py
@@ -36,9 +36,6 @@ lib.num_threads(8)
 
 prefactors_5p = np.array([1.0, -8.0, 8.0, -1.0]) / 12.0
 multipliers_5p = [-2, -1, 1, 2]
-# prefactors_9p = [1. / 280., -4. / 105., 1. / 5., -4. / 5.,
-#                  4. / 5., -1. / 5., 4. / 105., -1. / 280.]
-# multipliers_9p = [-4., -3., - 2., -1., 1., 2., 3., 4.]
 
 
 @dataclass
@@ -137,7 +134,7 @@ def main():
         "cvs-adc0",
         "cvs-adc1",
         "cvs-adc2",
-        # "cvs-adc2x",  # TODO: broken
+        "cvs-adc2x",
     ]
     molnames = [
         "h2o",